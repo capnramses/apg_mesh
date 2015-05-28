ced2404987db3eb69c3f5cc36c1b51798900a070
capnramses <lists@antongerdelan.net>
2015-05-28 19:22:21 +0100

    partially working blender export script

---
+++ blender_274/export_apg.py
@@ -0,0 +1,118 @@
+#
+# APG mesh format exporter (.apg)
+# 1st v 28 may 2015 anton gerdelan
+#
+
+bl_info = {
+	"name": "APG mesh format (.apg)",
+	"author": "Anton Gerdelan",
+	"version": (0, 1),
+	"blender": (2, 74, 0),
+	"location": "File > Import-Export > APG (.apg) ",
+	"description": "Import-Export APG Format",
+	"warning": "",
+	"wiki_url": ""
+				"",
+	"category": "Import-Export",
+}
+
+import bpy
+
+#v.co - vp
+#v.no - vn
+#mesh.face.uv[index]; - vt
+
+def faceToTriangles(face):
+	triangles = []
+	if len(face) == 4:
+		triangles.append([face[0], face[1], face[2]])
+		triangles.append([face[2], face[3], face[0]])
+	else:
+		triangles.append(face)
+
+	return triangles
+
+def faceValues(face, mesh, matrix):
+	fv = []
+	for verti in face.vertices:
+		fv.append((matrix * mesh.vertices[verti].co)[:])
+	return fv
+
+def faceToLine(face):
+	return "".join([("%.2f %.2f %.2f\n" % v) for v in face])
+
+def apg_write (context, filepath):
+	scene = context.scene
+
+	faces = []
+	for obj in context.selected_objects:
+		if obj.type != 'MESH':
+			try:
+				me = obj.to_mesh(scene, True, "PREVIEW")
+			except:
+				me = None
+			is_tmp_mesh = True
+		else:
+			me = obj.data
+			if not me.tessfaces and me.polygons:
+				me.calc_tessface()
+			is_tmp_mesh = False
+
+		if me is not None:
+			matrix = obj.matrix_world.copy()
+			for face in me.tessfaces:
+				fv = faceValues(face, me, matrix)
+				faces.extend(faceToTriangles(fv))
+
+			if is_tmp_mesh:
+				bpy.data.meshes.remove(me)
+
+	# write the faces to a file
+	file = open(filepath, "w")
+	file.write ("@Anton's mesh format v.27DEC2014. Blender export. https://github.com/capnramses/apg_mesh/\n")
+	vc = 3 * len(faces)
+	file.write ("@vert_count %i\n@vp_comps 3\n" % vc)
+	for face in faces:
+		file.write(faceToLine(face))
+	file.write ("@vn_comps 3\n")
+	file.write ("@vt_comps 2\n")
+	file.write ("@vtan_comps 4\n")
+	file.close()
+
+# ExportHlper is a hlper class, defines fname and invoke() which calls file
+# selector
+from bpy_extras.io_utils import ExportHelper
+from bpy.props import StringProperty, BoolProperty, EnumProperty
+from bpy.types import Operator
+
+class APGExporter(Operator, ExportHelper):
+	"""This is the tooltip/generated docs blurb"""
+	bl_idname = "export_data.apg_mesh"
+	bl_label = "Export APG"
+
+	filename_ext = ".apg"
+	filter_glob = StringProperty(
+		default="*.apg",
+		options={'HIDDEN'}
+		)
+
+	def execute(self, context):
+		apg_write(context, self.filepath)
+
+		return {'FINISHED'}
+
+def menu_export(self, context):
+	self.layout.operator(APGExporter.bl_idname, text="APG Mesh (.apg)")
+
+
+def register():
+	bpy.utils.register_class(APGExporter)
+	bpy.types.INFO_MT_file_export.append(menu_export)
+
+
+def unregister():
+	bpy.utils.unregister_class(APGExporter)
+	bpy.types.INFO_MT_file_export.remove(menu_export)
+
+if __name__ == "__main__":
+	register()
